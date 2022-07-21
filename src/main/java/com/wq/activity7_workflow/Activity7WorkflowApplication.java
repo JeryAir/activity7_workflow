cc650467f553a564daade7fbde9865af857be474
unknown <752527020@qq.com>
2022-07-21 15:46:59 +0800

    提交

---
+++ src/main/java/com/wq/activity7_workflow/Activity7WorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.wq.activity7_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity7WorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activity7WorkflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wq/activity7_workflow/Activity7WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wq.activity7_workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity7WorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
